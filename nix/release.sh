0e40b397fa1042e117ec0809d5715b7aa1e636f6
tkachuk-labs <tkachuk.labs@gmail.com>
2021-09-25 00:21:06 +0300

    add extra-substituters

--- nix/release.sh
+++ nix/release.sh
@@ -6,4 +6,6 @@ set -e
 
 nix-build ./nix/default.nix \
   --option sandbox false \
-  -v --show-trace
+  -v --show-trace \
+  --option extra-substituters "https://cache.nixos.org https://hydra.iohk.io https://all-hies.cachix.org" \
+  --option trusted-public-keys "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY= hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ= all-hies.cachix.org-1:JjrzAOEUsD9ZMt8fdFbzo3jNAyEWlPAwdVuHw4RD43k="
